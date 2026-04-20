32265dc133d75c7d9e13222568275d55a3f4fc75
Felipe <apollonik10@gmail.com>
2026-04-20 13:35:43 -0300

    fix: corrigindo caminho do service worker (PWA install fix)

--- pwa/js/sw.js
+++ pwa/js/sw.js
@@ -3,8 +3,8 @@
 export function registerSW() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/flor-de-lotus/pwa/service-worker.js', {
-        scope: '/flor-de-lotus/pwa/'
+      navigator.serviceWorker.register('./service-worker.js')
+
       })
         .then(() => console.log('SW registrado'))
         .catch(err => console.log('Erro SW:', err));
